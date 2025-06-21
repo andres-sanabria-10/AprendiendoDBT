03f074b2674b9b9511913e5945ac59748908d721
Andres Lesmes <sanabrialesmesandresfelipe@gmail.com>
2025-06-21 16:15:30 +0000

    Se creo el modelo estrella

--- models/mart/sales/fct_montly_sales.sql
+++ models/mart/sales/fct_montly_sales.sql
@@ -8,4 +8,4 @@ TOTAL_SALE,
 TOTAL_COMISION
 FROM {{ref("psg_montly_sales_make") }} as psg_m 
 left join {{ref("dim_carMake")}} as dim_cm on psg_m.CARMAKE = dim_cm.CARMAKE
-left join {{ref("dim_salesperson")}} as dim_sp on psg_m.CARMAKE = dim_sp.CARMAKE
+left join {{ref("dim_salesperson")}} as dim_sp on psg_m.SALESPERSON = dim_sp.SALESPERSON
